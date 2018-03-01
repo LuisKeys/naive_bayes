974e6a92ef4113e1ed16b1d3c368229b9ee4330f
lucho <luis.paradela@gmail.com>
2018-03-01 18:55:44 -0300

    PP

--- src/classifier.cpp
+++ src/classifier.cpp
@@ -20,29 +20,7 @@ GNB::~GNB() {}
 
 void GNB::train(vector<vector<double>> data, vector<string> labels)
 {
-
 	// Get mean and deviation
-	//Counter for the 3 labels values
-	std::vector<double> counter = {0.0, 0.0, 0.0}; //left, keep and right
-	std::vector<double> sum = {0.0, 0.0, 0.0, 0.0, 
-								0.0, 0.0, 0.0, 0.0,  //s, d, s', d' keep
-								0.0, 0.0, 0.0, 0.0, }; //s, d, s', d' right
-	std::vector<double> mean = {0.0, 0.0, 0.0, 0.0, 
-								0.0, 0.0, 0.0, 0.0,  //s, d, s', d' keep
-								0.0, 0.0, 0.0, 0.0, }; //s, d, s', d' right
-
-	std::vector<double> var = {0.0, 0.0, 0.0, 0.0, 
-								0.0, 0.0, 0.0, 0.0,  //s, d, s', d' keep
-								0.0, 0.0, 0.0, 0.0, }; //s, d, s', d' right
-
-	std::vector<double> sd = {0.0, 0.0, 0.0, 0.0, 
-								0.0, 0.0, 0.0, 0.0,  //s, d, s', d' keep
-								0.0, 0.0, 0.0, 0.0, }; //s, d, s', d' right
-
-	std::vector<string> names = {"s Left", "d Left", "s\' Left", "d\' Left", 
-								"s Keep", "d Keep", "s\' Keep", "d\' Keep", 
-								"s Right", "d Right", "s\' Right", "d\' Right"};
-
 	int offset = 4;
 	for(int i = 0; i < data.size(); ++i) {
 		if(labels[i] == "left"){
@@ -94,26 +72,16 @@ void GNB::train(vector<vector<double>> data, vector<string> labels)
 		for(int j = 0; j < 4; ++j) {
 			var[j + i * offset] /= counter[i];
 			sd[j + i * offset] = sqrt(var[j + i * offset]);
-				cout << "Value:" << names[j + i * offset] << " Mean:" << mean[j + i * offset] << " sd:" << sd[j + i * offset] << endl;
+				cout << "Val:" << names[j + i * offset] << " m:" << mean[j + i * offset] 
+				<< " sd:" << sd[j + i * offset] 
+				<< endl;
 		}
 	}
 
-	/*
-		Trains the classifier with N data points and labels.
-
-		INPUTS
-		data - array of N observations
-		  - Each observation is a tuple with 4 values: s, d, 
-		    s_dot and d_dot.
-		  - Example : [
-			  	[3.5, 0.1, 5.9, -0.02],
-			  	[8.0, -0.3, 3.0, 2.2],
-			  	...
-		  	]
-
-		labels - array of N labels
-		  - Each label is one of "left", "keep", or "right".
-	*/
+	for(int i = 0; i < 3; ++i) {
+		pp[i] = counter[i] / data.size();
+		cout << " pp:" << pp[i] << endl;
+	}
 }
 
 string GNB::predict(vector<double> sample)
--- src/classifier.h
+++ src/classifier.h
@@ -27,6 +27,36 @@ public:
  	void train(vector<vector<double> > data, vector<string>  labels);
 
   	string predict(vector<double>);
+  	
+	//Counter for the 3 labels values
+	std::vector<double> counter = {0.0, 0.0, 0.0}; //left, keep and right
+
+	// Sum by feature and label
+	std::vector<double> sum = {0.0, 0.0, 0.0, 0.0, 
+								0.0, 0.0, 0.0, 0.0,  //s, d, s', d' keep
+								0.0, 0.0, 0.0, 0.0, }; //s, d, s', d' right
+	// Mean by feature and label
+	std::vector<double> mean = {0.0, 0.0, 0.0, 0.0, 
+								0.0, 0.0, 0.0, 0.0,  //s, d, s', d' keep
+								0.0, 0.0, 0.0, 0.0, }; //s, d, s', d' right
+
+	// Variance by feature and label
+	std::vector<double> var = {0.0, 0.0, 0.0, 0.0, 
+								0.0, 0.0, 0.0, 0.0,  //s, d, s', d' keep
+								0.0, 0.0, 0.0, 0.0, }; //s, d, s', d' right
+	// Standard deviation by feature and label
+	std::vector<double> sd = {0.0, 0.0, 0.0, 0.0, 
+								0.0, 0.0, 0.0, 0.0,  //s, d, s', d' keep
+								0.0, 0.0, 0.0, 0.0, }; //s, d, s', d' right
+
+	// Prior probability
+	std::vector<double> pp = {0.0, 0.0, 0.0, 0.0, 
+								0.0, 0.0, 0.0, 0.0,  //s, d, s', d' keep
+								0.0, 0.0, 0.0, 0.0, }; //s, d, s', d' right
+
+	std::vector<string> names = {"s Left", "d Left", "s\' Left", "d\' Left", 
+								"s Keep", "d Keep", "s\' Keep", "d\' Keep", 
+								"s Right", "d Right", "s\' Right", "d\' Right"};
 
 };
 
